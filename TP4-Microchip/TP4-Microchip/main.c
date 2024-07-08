a711c38348e570c60ffb33d89c2dcad660d73469
santi-nihany <santi.nihany@gmail.com>
2024-07-08 13:45:14 -0300

    potenciometro + adc

--- TP4-Microchip/TP4-Microchip/main.c
+++ TP4-Microchip/TP4-Microchip/main.c
@@ -5,17 +5,36 @@
 #include "serialPort.h"
 
 volatile char RX_Buffer=0;
+uint8_t adc_value;
+
+void ADC_Init() {
+	DIDR0 = 0x01; // digital input disable
+	ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2) | (1 << ADEN) ; // adc enable and ck/128 -> f = 125KHz
+	ADMUX |= (1 << REFS0) | (1 << ADLAR) | (1 << MUX1) | (1 << MUX0); // Vref = AVCC, left justified, AC3 pin
+}
+
+uint16_t ADC_Read() {
+	ADCSRA |= (1 << ADSC);  // Iniciar conversión
+	while (!(ADCSRA & (1 << ADSC)));  // Esperar a que termine la conversión
+	ADCSRA |= (1<< ADIF); // clear the ADIF flag
+	return ADCH;  // Retornar el valor leído
+}
+
 
 int main(void)
 {
 	uint8_t l_red = 0;
-	DDRB = (1 << PORTB1)|(1 << PORTB2)|(1 << PORTB5); // salida puertos B 1,2,5
+	uint8_t act = 0;
+	DDRB |= (1 << PORTB1)|(1 << PORTB2)|(1 << PORTB5); // salida puertos B 1,2,5
+	DDRC &= ~(1<< PINC3);
 	/* configuracion pwm puertos 1 y 2*/
 	OCR1A = 0;
 	OCR1B = 0;
-	TCCR1A = (1<< COM1A1) | (1<< COM1B1) | (1<< COM1A0) | (1<< COM1B0) | (1<< WGM10);	// PHASE CORRECT 8BIT - INVERTIDO
-	TCCR1B= (1<< CS10);							        // PRESCALER 1
+	TCCR1A |= (1<< COM1A1) | (1<< COM1B1) | (1<< COM1A0) | (1<< COM1B0) | (1<< WGM10);	// PHASE CORRECT 8BIT - INVERTIDO
+	TCCR1B |= (1<< CS10);							        // PRESCALER 1
 	PORTB|=((1 << PORTB2)|(1 << PORTB5));
+
+	ADC_Init();
 	
 	/* UART */
 	SerialPort_Init(BR9600);
@@ -33,19 +52,23 @@ int main(void)
 		}
 		// leer
 		if(RX_Buffer){
-			switch (RX_Buffer) {
-				case 'R':
-					l_red^=0xFF;
-					break;
-				case 'G':
-					OCR1B^=0xFF;
-					break;
-				case 'B':
-					OCR1A^=0xFF;
-					break;
-					
-			}
+			act = RX_Buffer;
 			RX_Buffer=0;
+			SerialPort_Send_Data(act);
+			while(!RX_Buffer){
+				adc_value = ADC_Read();	
+				switch (act) {
+					case 'R':
+						l_red = adc_value;
+					break;
+					case 'G':
+						OCR1B = adc_value;
+					case 'B':
+						OCR1A = adc_value;
+					break;
+				}
+				_delay_ms(100);
+			}
 		}
     }
 }
